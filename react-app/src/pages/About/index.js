3b61073f63b3310a8511a226042909e4e74c93fb
Shaul Daon <daonsh@gmail.com>
2024-01-21 16:36:57 +0200

    first commit, changed menus and screens for new interface

---
+++ react-app/src/pages/About/index.js
@@ -0,0 +1,12 @@
+
+import {
+  Box
+} from "@mui/material";
+
+export default function About() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      About
+    </Box>
+  );
+}
---
+++ react-app/src/pages/Contact/index.js
@@ -0,0 +1,12 @@
+import {
+  Box
+} from "@mui/material";
+
+export default function Contact() {
+  
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      Contact
+    </Box>
+  );
+}
